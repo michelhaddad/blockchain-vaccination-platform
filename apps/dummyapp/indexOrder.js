13307c1c2c80f76b3acdcc4574e0338f0ab208d9
Michelk-byte <michel.khoury.w@hotmail.com>
2021-04-13 15:37:34 +0300

    update config.json

--- apps/dummyapp/indexOrder.js
+++ apps/dummyapp/indexOrder.js
@@ -29,7 +29,7 @@ async function main() {
             config.connection_profile = config.connection_profile.replace("basic", process.env.NETWORK);
         }
         if ( process.env.CHANNEL != undefined) {
-            config.channel.channelName = config.channel.channelName.replace("mychannel", process.env.CHANNEL);
+            config.channel.channelName = config.channel.channelName.replace("contractchannel", process.env.CHANNEL);
         }
 
         ccpPath = path.resolve(__dirname, config.connection_profile);
