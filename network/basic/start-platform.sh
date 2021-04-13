30d545c9dbb0491b9319006a89b413fdb2e5e7e6
MichelK-Byte <michel.khoury.w@hotmail.com>
2021-04-14 00:10:46 +0300

    fix typo channel -> network

--- network/basic/start-platform.sh
+++ network/basic/start-platform.sh
@@ -4,7 +4,7 @@ export GOPATH=$HOME/go
 export GOBIN=$GOPATH/bin
 export PATH=$PATH:$GOBIN
 export FABRIC_CFG_PATH=$PWD
-export CHANNEL_NAME=supplychainchannel
+export NETWORK_NAME=supplychainnetwork
 
 mkdir -p ../../dashboards/7/dashboard
 
@@ -14,7 +14,7 @@ echo "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
 echo "LAUNCHING NETWORK"
 echo "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
 
-COMPOSE_PROJECT_NAME=supplychainchannel docker-compose -f docker-compose.yaml up -d
+COMPOSE_PROJECT_NAME=supplychainnetwork docker-compose -f docker-compose.yaml up -d
 
 sleep 15
 docker exec -it cli scripts/platformcc/channel-chaincode-setup.sh
