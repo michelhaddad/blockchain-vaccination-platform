de5cce0723d6d425d95881c1e9d96d7982f219a7
michelhaddad <michhadd1@gmail.com>
2021-04-16 13:21:59 +0300

    Feat: add order and hospitals endpoints in backend

--- apps/vaccination-web-api/routes/routes.js
+++ apps/vaccination-web-api/routes/routes.js
@@ -2,6 +2,8 @@
 module.exports = function (app) {
 
   const donationsController = require('../controllers/donationController');
+  const orderController = require('../controllers/orderController');
+  const hospitalController = require('../controllers/hospitalController');
 
   app.route('/donations')
     .get(donationsController.getAllDonations);
@@ -10,6 +12,26 @@ module.exports = function (app) {
   app.route('/donations/redeem')
     .put(donationsController.redeem);
   app.route('/user/donations')
-    .get(donationsController.getUserDonations)
+    .get(donationsController.getUserDonations);
+
+  app.route('/orders')
+  .get(orderController.getAllOrders);
+  app.route('/orders')
+  .post(orderController.issueOrder);
+  app.route('/orders/:id/approve')
+  .put(orderController.approveOrder);
+  app.route('/orders/:id/reject')
+  .put(orderController.rejectOrder);
+  app.route('/orders/:id/ship')
+  .put(orderController.setOrderShipped);
+  app.route('/orders/:id/deliver')
+  .put(orderController.setOrderDelivered);
+
+  app.route('/hospitals')
+  .get(hospitalController.getAllHospitals);
+  app.route('/hospitals/:id/deliver')
+  .put(hospitalController.deliverVials);
+  app.route('/hospitals/:id/inoculate')
+  .put(hospitalController.inoculatePatients);
 
 };
